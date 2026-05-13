8d3efd3ed9f74bdd371471a2db140308b4b26403
Abdullah-kwl <mrabdullahkwl@gmail.com>
2026-05-13 23:14:51 +0500

    added memory to angent

--- app/ai_layer/agent.py
+++ app/ai_layer/agent.py
@@ -2,19 +2,31 @@
 with the language model using a LangGraph based approach."""
 
 from langgraph.graph import StateGraph, START, END
-from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import InMemorySaver
+
+# from langchain_core.messages import HumanMessage
 from app.ai_layer.graph_states import GraphState
 from app.ai_layer.nodes import chat_with_model
+
+checkpoint_saver = InMemorySaver()
 
 graph = StateGraph(GraphState)
 graph.add_node("chat", chat_with_model)
 graph.add_edge(START, "chat")
 graph.add_edge("chat", END)
 
-chatbot = graph.compile()
+chatbot = graph.compile(checkpointer=checkpoint_saver)
 
-if __name__ == "__main__":
-    # print(chatbot.get_graph().draw_ascii())
-    initial_state = {"messages": [HumanMessage(content="what is the meaning of life?")]}
-    result = chatbot.invoke(initial_state)
-    print(result)
+# if __name__ == "__main__":
+#     # print(chatbot.get_graph().draw_ascii())
+#     config={"configurable": {"thread_id": 1}}
+#     initial_state = {"messages": [HumanMessage(content="my name is ali i like icecreame specially chocolate flavor")]}
+#     result = chatbot.invoke(initial_state, config=config)
+#     # print(result)
+#     print(result["messages"][-1].content)
+#     print("----"*10)
+
+#     initial_state = {"messages": [HumanMessage(content="what you know about me")]}
+#     result = chatbot.invoke(initial_state, config=config)
+#     # print(result)
+#     print(result["messages"][-1].content)
