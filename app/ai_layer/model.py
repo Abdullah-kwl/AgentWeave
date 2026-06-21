c56d71b1c44703beddb6cd5c38121322be81fa6d
Abdullah-kwl <mrabdullahkwl@gmail.com>
2026-06-21 08:32:09 +0500

    minor changes

--- app/ai_layer/model.py
+++ app/ai_layer/model.py
@@ -1,12 +1,12 @@
 """This module initializes the language models used in the AI layer of the application."""
 
 from langchain_groq import ChatGroq
-from app.utils.config import secrets, config
+from app.utils.config import secrets, llm_config
 
 
 def initialize_model():
     """Initializes the ChatGroq model with the provided API key."""
-    return ChatGroq(model=config.model, groq_api_key=secrets.GROQ_API_KEY)
+    return ChatGroq(model=llm_config.model, groq_api_key=secrets.GROQ_API_KEY)
 
 
 def initialize_tool_enabled_model(model, tools):
